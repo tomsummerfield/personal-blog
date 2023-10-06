0f5a7d6ced530dde3952f86bb601b1fd22716168
Thomas Summerfield <summerfield11@hotmail.co.uk>
2023-10-06 11:25:25 +0100

    Added contentful api connection

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,24 +1,72 @@
+"use client";
 import React from "react";
-import Link from "next/link";
+import { Link } from "react-scroll";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
+  const router = usePathname();
+  const slug = router.substring("/blog/".length);
+
   return (
     <footer id="footer" className="bg-[#000E2B]">
-      <div id="container" className="px-[1rem] py-[1.4rem]">
-        <h3 className="text-white font-semibold">Tom Summerfield</h3>
-        <ul className="flex mt-[2rem] gap-[1.25rem]">
-          <li>
-            <Link href="/" className="text-white font-light">HOME</Link>
-          </li>
-          <li>
-            <Link href="/" className="text-white font-light">BLOG</Link>
-          </li>
-          <li>
-            <Link href="/" className="text-white font-light">CONTACT</Link>
-          </li>
-        </ul>
-        <div className="border-[0.5px] border-b-[#fafafa33] mt-[1.8rem]"></div>
-        <p className="mt-3 text-white font-light">
+      <div
+        id="container"
+        className="px-[1rem] py-[1.4rem] mx-auto max-w-screen-2xl md:px-[6rem] md:py-[6rem]"
+      >
+        <div className="md:flex md:items-center md:justify-between">
+          <h3 className="text-white font-semibold md:text-[22px]">
+            Tom Summerfield
+          </h3>
+          <ul className="flex mt-[2rem] gap-[1.25rem] md:mt-0 md:gap-[3rem]">
+            <li>
+              {slug.length > 0 ? (
+                <a href="/" className="text-white font-light md:text-[20px] hover:text-blue-500">HOME</a>
+              ) : (
+                <Link
+                  to="hero"
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  className="text-white font-light md:text-[20px] hover:text-blue-500"
+                >
+                  HOME
+                </Link>
+              )}
+            </li>
+            <li>
+              {slug.length > 0 ? (
+                <a href="/" className="text-white font-light md:text-[20px] hover:text-blue-500">BLOGS</a>
+              ) : (
+                <Link
+                  to="blogs"
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  className="text-white font-light md:text-[20px] hover:text-blue-500"
+                >
+                  BLOGS
+                </Link>
+              )}
+            </li>
+            <li>
+              {slug.length > 0 ? (
+                <a href="/" className="text-white font-light md:text-[20px] hover:text-blue-500">CONTACT</a>
+              ) : (
+                <Link
+                  to="contact"
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  className="text-white font-light md:text-[20px] hover:text-blue-500"
+                >
+                  CONTACT
+                </Link>
+              )}
+            </li>                        
+          </ul>
+        </div>
+        <div className="border-[0.5px] border-b-[#fafafa33] mt-[1.8rem] md:block"></div>
+        <p className="mt-3 text-white font-light md:mt-7 md:text-[20px]">
           Copyright ©2023 Tom Summerfield
         </p>
       </div>
